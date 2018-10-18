3578908ba91598a5298c090350ea27c3401cb4bd
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-10-18 16:49:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,7 @@ client.on('message', async message => {
     var filter = m => m.author.id === message.author.id;// Alpha Codes
     var subChannel = message.guild.channels.find(c => c.name === 'support-join');// Alpha Codes
    
-    if(command == prefix + 'join-support') {// Alpha Codes
+    if(command == prefix + 'support') {// Alpha Codes
         if(message.author.bot) return;
         if(message.channel.type === 'dm') return;
  
