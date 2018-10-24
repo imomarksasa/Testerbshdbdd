80fce50c92035b4fae562e2f9fdbfc7aad59a4e4
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2018-10-24 15:16:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,19 @@ client.on('message', message => {
 });
 
 
-
+client.on("guildMemberAdd", msg => {
+  var AlphaE = new Discord.RichEmbed()
+.setColor("RANDOM")
+.setAuthor(msg.user.username, msg.user.avatarURL)
+.setThumbnail(msg.user.avatarURL)
+.setTitle(`
+AsH افضل سيرفر حياة واقعية لاتحرمنا من تواجدك
+`)
+.addField("هذي الرساله خاصه بك يا قلبي", `${msg.user.tag}`, true)
+.addField(`هذا الرابط خاص بك`,`https://discord.gg/ZpbAQ2E`, true)
+ .setFooter(msg.user.tag, msg.user.avatarURL, true)
+msg.user.sendMessage(AlphaE);
+});
 
 
 
